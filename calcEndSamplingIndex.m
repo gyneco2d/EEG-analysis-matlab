0bb6e4a93e659d3b48300fbeef241197a71cb4bd
gyneco2d <mikeda2501@gmail.com>
2019-12-10 15:16:44 +0900

    Tweak

--- calcEndSamplingIndex.m
+++ calcEndSamplingIndex.m
@@ -1,19 +1,22 @@
+% Calculate index for dataset split.
+
+% Initialize sampling frequency
+fs = 2048;
+
 prompt = '`ALLEEG.event(any).latency` value here: ';
 firstIndex = input(prompt);
 if isempty(firstIndex)
-    disp('Invalid input');
-end
-
-prompt = 'Sampling frequency [default: 2048]: ';
-fs = input(prompt);
-if isempty(fs)
-    fs = 2048;
+    error('Input is required');
+elseif ~isnumeric(firstIndex)
+    error('Input must be a numeric');
 end
 
 prompt = 'How many seconds later [default: 200sec]: ';
 interval = input(prompt);
 if isempty(interval)
     interval = 200;
+elseif ~isnumeric(interval)
+    error('Input must be a numeric');
 end
 
 lastIndex = (firstIndex - 1) + fs * interval;
