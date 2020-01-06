ef6dd2a3760387b051af71b470c74e6ee223f14e
gyneco2d <mikeda2501@gmail.com>
2020-01-06 20:57:01 +0900

    Create new plotting function

--- +constants/@ProjectConstants/ProjectConstants.m
+++ +constants/@ProjectConstants/ProjectConstants.m
@@ -3,5 +3,6 @@ classdef ProjectConstants
         AlphaBand = [8:13];
         FFTInterval = 2;
         SmoothingWindowSize = 30;
+        LatterHalfDataIndex = [5:8];
     end
 end
---
+++ compareByState.m
@@ -0,0 +1,33 @@
+function compareByState(AlphaEEG, channel)
+    % compareByState() - Detrend through state & plot AlphaEEG power for each state
+    %
+    % Usage:
+    %   >> compareByState( AlphaEEG, [14:18] );
+    %
+    % Inputs:
+    %   AlphaEEG - [structure] structure created by collectAlpha()
+    %   channel  - [integer array] electrode number used for calc & plotting
+
+    % Confirm args
+    if ~exist('channel', 'var'); channel = [14:18]; end
+
+    import constants.ProjectConstants;
+
+    arr = [];
+    for iState = ProjectConstants.LatterHalfDataIndex
+        arr = [arr mean(AlphaEEG(iState).rootmean(channel))];
+    end
+
+    figure;
+    state = [];
+    for iState = ProjectConstants.LatterHalfDataIndex
+        name = strsplit(AlphaEEG(iState).setname, ' - ');
+        state = [state name(2)];
+    end
+    bar(ProjectConstants.LatterHalfDataIndex, detrend(arr) + mean(arr));
+    xticks(ProjectConstants.LatterHalfDataIndex);
+    xticklabels(state);
+    xlabel('State');
+    ylabel('Power[uV]');
+    title('AlphaEEG power per state');
+end
