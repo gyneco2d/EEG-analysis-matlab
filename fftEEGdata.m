fdbd6378cb26befb78031432af0549d56d26cb1c
gyneco2d <mikeda2501@gmail.com>
2020-01-30 06:34:02 +0900

    Update EEGFREQS structure

--- fftEEGdata.m
+++ fftEEGdata.m
@@ -8,17 +8,16 @@ function [EEGFREQS] = fftEEGdata(ALLEEG)
     %   ALLEEG - [structure] EEGLAB dataset structure
     %
     % structure array:
-    %   EEGFREQS setname                      - dataset name
-    %            axis                         - frequency axis
-    %            distribution                 - EEG power for each frequency 
-    %            timeseries_distribution      - time series EEG power for each frequency
-    %            percentage                   - EEG percentages
-    %            timeseries_percentage        - timeseries EEG percentages
-    %            raw                          - alpha waves in all fft windows
-    %            section_power                - square root of the averaged alpha waves in the section
-    %            timeseries_power             - square root of the averaged alpha waves for each fft window
-    %            normalized                   - normalized alpha waves (respect to raw average)
-    %            normalized_section_power     - normalized section_power (respect to raw average)
+    %   EEGFREQS setname                          - dataset name
+    %            axis                             - frequency axis
+    %            distribution                     - EEG power for each frequency 
+    %            timeseries_distribution          - time series EEG power for each frequency
+    %            percentage                       - EEG percentages
+    %            timeseries_percentage            - timeseries EEG percentages
+    %            timeseries_{each EEG}            - square root of average EEG waves for fft window
+    %            section_{each EEG}               - average of timeseries_{each EEG}_power for section
+    %            normalized_timeseries_{each EEG} - normalized section_{each EEG}_power (respect to raw average)
+    %            normalized_section_{each EEG}    - normalized alpha waves (respect to raw average)
 
     % Import constants
     import('constants.ProjectConstants');
@@ -30,7 +29,14 @@ function [EEGFREQS] = fftEEGdata(ALLEEG)
                     ProjectConstants.BioSemiSamplingRate;
         nComponent = fix(totalTime - 1);
         stepsize = n / 2;
-        alphaIndex = calcFreqIndex(ProjectConstants.AlphaWaves, f);
+        thetaIndex = getIndexOfFreqAxis(ProjectConstants.ThetaWaves, f);
+        alphaIndex = getIndexOfFreqAxis(ProjectConstants.AlphaWaves, f);
+        betaIndex = getIndexOfFreqAxis(ProjectConstants.BetaWaves, f);
+        gammaIndex = getIndexOfFreqAxis(ProjectConstants.GammaWaves, f);
+        thetaEEG = zeros(32, length(thetaIndex)*nComponent);
+        alphaEEG = zeros(32, length(alphaIndex)*nComponent);
+        betaEEG = zeros(32, length(betaIndex)*nComponent);
+        gammaEEG = zeros(32, length(gammaIndex)*nComponent);
 
         % Initialize EEGFREQS structure
         EEGFREQS(section).setname = ALLEEG(section).setname;
@@ -39,9 +45,14 @@ function [EEGFREQS] = fftEEGdata(ALLEEG)
         EEGFREQS(section).timeseries_distribution(nComponent) = {[]};
         EEGFREQS(section).percentage = struct();
         EEGFREQS(section).timeseries_percentage = struct();
-        EEGFREQS(section).raw = zeros(32, length(alphaIndex)*nComponent);
-        EEGFREQS(section).section_power = zeros(32, 1);
-        EEGFREQS(section).timeseries_power = zeros(32, nComponent);
+        EEGFREQS(section).section_theta = zeros(32, 1);
+        EEGFREQS(section).section_alpha = zeros(32, 1);
+        EEGFREQS(section).section_beta = zeros(32, 1);
+        EEGFREQS(section).section_gamma = zeros(32, 1);
+        EEGFREQS(section).timeseries_theta = zeros(32, nComponent);
+        EEGFREQS(section).timeseries_alpha = zeros(32, nComponent);
+        EEGFREQS(section).timeseries_beta = zeros(32, nComponent);
+        EEGFREQS(section).timeseries_gamma = zeros(32, nComponent);
 
         for channel = ProjectConstants.AllElectrodes
             for iComponent = 1:nComponent
@@ -62,20 +73,29 @@ function [EEGFREQS] = fftEEGdata(ALLEEG)
                         power]...
                     };
 
-                % Save unprocessed Alpha wave power
-                for index = 1:length(alphaIndex)
-                    EEGFREQS(section).raw(channel, index + (iComponent-1)*length(alphaIndex)) = ...
-                        power(alphaIndex(index));
-                end
-                % Collect alpha wave power for each fft window
-                EEGFREQS(section).timeseries_power(channel, iComponent) = ...
+                % Collect each EEG wave power for each fft window
+                EEGFREQS(section).timeseries_theta(channel, iComponent) = ...
+                    sqrt(mean(power(thetaIndex)));
+                EEGFREQS(section).timeseries_alpha(channel, iComponent) = ...
                     sqrt(mean(power(alphaIndex)));
+                EEGFREQS(section).timeseries_beta(channel, iComponent) = ...
+                    sqrt(mean(power(betaIndex)));
+                EEGFREQS(section).timeseries_gamma(channel, iComponent) = ...
+                    sqrt(mean(power(gammaIndex)));
             end
             % Calculate the average frequency distribution between fft window
             EEGFREQS(section).distribution(channel, :) = ...
                 EEGFREQS(section).distribution(channel, :) / nComponent;
-            EEGFREQS(section).section_power(channel, 1) = ...
-                sqrt(mean(EEGFREQS(section).raw(channel, :)));
+
+            % Calculate section average of each EEG wave power
+            EEGFREQS(section).section_theta(channel, 1) = ...
+                mean(EEGFREQS(section).timeseries_theta(channel, :));
+            EEGFREQS(section).section_alpha(channel, 1) = ...
+                mean(EEGFREQS(section).timeseries_alpha(channel, :));
+            EEGFREQS(section).section_beta(channel, 1) = ...
+                mean(EEGFREQS(section).timeseries_beta(channel, :));
+            EEGFREQS(section).section_gamma(channel, 1) = ...
+                mean(EEGFREQS(section).timeseries_gamma(channel, :));
         end
 
         % Calculate the percentage of each EEG
@@ -101,11 +121,12 @@ function [EEGFREQS] = fftEEGdata(ALLEEG)
         end
 
         % Create normalized data
-        standard = mean(EEGFREQS(section).raw, 'all');
-        EEGFREQS(section).normalized = EEGFREQS(section).raw / standard;
+        standard = mean(EEGFREQS(section).timeseries_alpha, 'all');
+        EEGFREQS(section).normalized_timeseries_alpha = ...
+            EEGFREQS(section).timeseries_alpha / standard;
         for channel = ProjectConstants.AllElectrodes
-            EEGFREQS(section).normalized_section_power(channel, 1) = ...
-                sqrt(mean(EEGFREQS(section).normalized(channel, :)));
+            EEGFREQS(section).normalized_section_alpha(channel, 1) = ...
+                mean(EEGFREQS(section).normalized_timeseries_alpha(channel, :));
         end
     end
 end
