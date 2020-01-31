2ee65cd3dc3ae521cf90135e568d59046d87e50b
gyneco2d <mikeda2501@gmail.com>
2020-01-31 23:37:31 +0900

    Fix & Add optional args

--- plotters/plotEEGpercentagesInTimeSeries.m
+++ plotters/plotEEGpercentagesInTimeSeries.m
@@ -1,36 +1,59 @@
-function plotEEGpercentagesInTimeSeries(AlphaEEG)
-    % plotEEGpercentageInTimeSeries() - plot EEG & Alpha wave percentage transition
+function plotEEGpercentagesInTimeSeries(EEGFREQS, wave)
+    % plotEEGpercentageInTimeSeries() - plot EEG percentage & each wave transition
     %
     % Usage:
-    %  >> plotEEGpercentageInTimeSeries( AlphaEEG );
+    %  >> plotEEGpercentageInTimeSeries( EEGFREQS );
     %
     % Inputs:
-    %   AlphaEEG - [structure] structure created by collectAlphaWaves()
+    %   EEGFREQS - [structure] structure created by fftEEGdata()
+    %   wave     - [string: 'all' / 'theta' / 'alpha' / 'beta' / 'gamma']
+    %              specify EEG wave to plot
 
+    import('constants.ProjectConstants');
+
+    % Confirm args
+    if length(EEGFREQS) ~= 1, error('Required 1 EEGFREQS'); end
+    if ~exist('wave', 'var'), wave = 'all'; end
+    if ~any(strcmp(wave, {'all', 'theta', 'alpha', 'beta', 'gamma'}))
+        error('Invalid EEG wave name');
+    end
+
+    waves = {'theta', 'alpha', 'beta', 'gamma'};
     percentages = [];
-    pctAlpha = [];
-    for component = 1:length(AlphaEEG(1).timeseries_eeg_percentage)
+    pctWaves = zeros(length(waves), 0);
+    for component = 1:length(EEGFREQS.timeseries_percentage)
         percentages = [
             percentages;
-            AlphaEEG(1).timeseries_eeg_percentage(component).theta...
-            AlphaEEG(1).timeseries_eeg_percentage(component).alpha...
-            AlphaEEG(1).timeseries_eeg_percentage(component).beta...
-            AlphaEEG(1).timeseries_eeg_percentage(component).gamma...
+            EEGFREQS(1).timeseries_percentage(component).theta...
+            EEGFREQS(1).timeseries_percentage(component).alpha...
+            EEGFREQS(1).timeseries_percentage(component).beta...
+            EEGFREQS(1).timeseries_percentage(component).gamma...
         ];
-        pctAlpha = [pctAlpha AlphaEEG(1).timeseries_eeg_percentage(component).alpha];
+        for index = 1:length(waves)
+            pctWaves(index, component) = EEGFREQS.timeseries_percentage(component).(char(waves(index)));
+        end
     end
 
     figure();
     bar(percentages, 'stacked');
-    legend({'theta', 'alpha', 'beta', 'gamma'}, 'location', 'northeast');
+    legend({'Theta', 'Alpha', 'Beta', 'Gamma'}, 'location', 'northeast');
     ylim([0 100]);
     xlabel('Time');
     ylabel('Percentage [%]');
-    title('EEG percentage in time series');
+    title('EEG percentage transition');
 
     figure();
-    plot(movmean(pctAlpha, constants.ProjectConstants.SmoothingWindowSize));
+    hold('on');
+    if strcmp(wave, 'all')
+        for index = 1:size(pctWaves, 1)
+            plot(movmean(pctWaves(index, :), ProjectConstants.SmoothingWindowSize));
+        end
+        title('EEG percentage transition');
+    else
+        index = strmatch(wave, waves);
+        plot(movmean(pctWaves(index, :), ProjectConstants.SmoothingWindowSize));
+        title([upper(wave(1)) wave(2:end) ' EEG percentage transition']);
+    end
     xlabel('Time');
     ylabel('Percentage [%]');
-    title('Alpha EEG percentage in time series');
 end
