87d8f1566462db726183951098c94ddc4b681af5
gyneco2d <mikeda2501@gmail.com>
2020-02-01 14:42:36 +0900

    Enable to plot specified wave

--- plotters/plotTransitionWithSPL.m
+++ plotters/plotTransitionWithSPL.m
@@ -1,40 +1,61 @@
-function plotTransitionWithSPL(EEGFREQS, audiofile, channel)
+function plotTransitionWithSPL(EEGFREQS, audiofile, wave, channel)
     % plotTransitionWithSPL() - Plot SPL(sound pressure level) and EEG transition
     %
     % Usage:
-    %   >> plotTransitionWithSPL( EEGFREQS, audiofile, channel );
+    %   >> plotTransitionWithSPL( EEGFREQS, '/User/gyneco2d/Documents/MATLAB/audio.wav',
+    %                           channel );
     %
     % Inputs:
-    %   channel - [integer array] electrode number used for plot
+    %   EEGFREQS  - [structure] structure created by fftEEGdata()
+    %   audiofile - [string] audio file path
+    %   wave      - [string: 'all' / 'theta' / 'alpha' / 'beta' / 'gamma'] specify EEG wave to plot
+    %   channel   - [integer array] electrode number used for plot
     %
     % Required files:
-    %   [mat] - continuous data of listening state
-    %   [wav] - continuous sound record of listening state
+    %   [wav] - continuous sound record of listening section
 
     % Import constants
     import('constants.ProjectConstants');
 
+    waves = {'theta', 'alpha', 'beta', 'gamma'};
+
     % Confirm args
-    if ~exist('plotchannel', 'var')
+    if length(EEGFREQS) ~= 1, error('Required 1 EEGFREQS'); end
+    if ~exist(audiofile, 'file'), error('Audio file does not exist'); end
+    if ~exist('wave', 'var'), wave = 'alpha'; end
+    if ~any(strcmp(wave, {'all', 'theta', 'alpha', 'beta', 'gamma'}))
+        error('Invalid EEG wave name');
+    end
+    if ~exist('channel', 'var')
         channel = ProjectConstants.OccipitalElectrodes;
     end
 
     % Read audio file
     [audio, Fs] = audioread(audiofile);
 
-    % Smoothing
-    smoothspl = movmean(abs(audio), Fs * ProjectConstants.SmoothingWindowSize);
-    for channel = plotchannel
-        smoothpower(channel, :) = movmean(AlphaEEG(1).timeseries_power(channel, :), ProjectConstants.SmoothingWindowSize);
+    waveTrans = [];
+    for iWave = waves
+        waveTrans = [waveTrans; mean(EEGFREQS.(['timeseries_', char(iWave)])(channel, :))];
     end
+    smoothspl = movmean(abs(audio), Fs * ProjectConstants.SmoothingWindowSize);
 
     % Plot
     figure;
     subplot(211);
-    hold on;
-    for channel = plotchannel
-        plot(smoothpower(channel, :));
+    hold('on');
+    if strcmp(wave, 'all')
+        for index = 1:length(waves)
+            plot(movmean(waveTrans(index, :), ProjectConstants.SmoothingWindowSize));
+        end
+        title('EEG transition');
+        legend();
+    else
+        index = find(strncmp(wave, waves, length(wave)));
+        plot(movmean(waveTrans(index, :), ProjectConstants.SmoothingWindowSize));
+        title([upper(wave(1)) wave(2:end) ' EEG transition']);
     end
+    xlabel('Time');
+    ylabel('Power [uV]');
     subplot(212);
     plot(smoothspl);
 end
